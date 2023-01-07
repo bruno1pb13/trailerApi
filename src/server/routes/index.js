eda0c5f3cb8cd6ea664f0bc792759ce638e85888
Bruno Santos <bruno1pb13@gmail.com>
2023-01-07 09:58:48 -0300

    importa todos os arquivos da pasta routes de uma vez

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,7 +1,10 @@
+const fs = require('fs')
+const path = require('path')
 
-module.exports = (app)=>{
-    //Import here the server routes
-    
-    require('./tmdb')(app) // server data info
-    
-    }
+
+module.exports = app => {
+    fs
+    .readdirSync(__dirname)
+    .filter(file => ((file.indexOf('.')) !== 0 && (file !== 'index.js') ))
+    .forEach(file => require(path.resolve(__dirname, file))(app))
+}
